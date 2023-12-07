8b06550a3fe4a7631b3557161c6b99daca0fa89c
Levi Santos <135247614+levisantosfsl@users.noreply.github.com>
2023-12-07 16:06:16 -0300

    Update utils.js

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,6 +1,6 @@
 import  HomePage  from '../pos/homepage'
 import  LoginPage from '../pos/loginpage'
-import ShopPage from '../pos/shopPage'
+import ShopPage from '../pos/shoppage'
 
 class Utils {
     wait(timer){
